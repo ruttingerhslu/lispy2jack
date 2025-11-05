710261a38cfba4882ad0f9363be68da70255da61
Rafael Uttinger <rafael.uttinger@stud.hslu.ch>
2025-11-05 18:18:29 +0100

    fix: move fjack test file

--- src/__main__.py
+++ src/__main__.py
@@ -4,14 +4,13 @@ def main(prompt: str = "fjack.py> "):
     """A prompt read-eval-print loop."""
     while True:
         ast = parse(input(prompt))
-        print(ast)
         cps = v(ast)
         collect_phi_assignments(cps)
         ssa = g_proc(v(ast))
 
         print(f'Scheme: {ast}')
         print(f'CPS: {cps}')
-        print(f'Phi assignments: {phi_assignments}')
+        # print(f'Phi assignments: {phi_assignments}')
         print(f'SSA: {ssa}')
 
 if __name__ == "__main__":
